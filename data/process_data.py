b6ac6e6bb12d10bcd7b232913249f422965f1924
SoniaFYLin <admost0118@gmail.com>
2022-03-26 19:28:56 -0700

    added load_data, clean data, save_data

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,59 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+
+
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = pd.merge(messages, categories, on="id", how="outer")
+    return df
 
 
 def clean_data(df):
-    pass
 
+    ## create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';',expand=True)
+
+    ## Extract category columns
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # convert category values to just number 0 or 1
+    df_categories = categories
+    for column in df_categories:
+        # set each value to be the last character of the string
+        # convert column from string to numeric
+        df_categories[column] = df_categories[column].apply(lambda x: int(x[-1]))
+
+    ## Replace categories column in df with new category columns
+    # drop the original categories column from `df`
+    df.drop('categories', inplace=True, axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df_concat = pd.concat([df, df_categories], axis=1)
+
+    ## remove duplicates
+    # check number of duplicates
+    print(df_concat.duplicated().sum())
+
+    # drop duplicates
+    df_concat_drop = df_concat.drop_duplicates()
+
+    return df_concat_drop
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///disaster_response.db')
+    df.to_sql(database_filename, engine, index=False)
 
 
 def main():
